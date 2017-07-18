2236edf4413a86abe7ef8396278d40c000095647
mypigmylove <yxxb@163.com>
2017-07-18 20:48:00 +0800

    新增thread代码 by yangxing

---
+++ MultiThreads/Threads/Models/ExtendMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threads.Models
+{
+    public static class ExtendMethod
+    {
+        public static ThreadState SimpleState(this ThreadState th)
+        {
+            return th&(ThreadState.Running|ThreadState.Wait|ThreadState.Standby);
+        }
+    }
+}
--- MultiThreads/Threads/Program.cs
+++ MultiThreads/Threads/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Threads
@@ -10,6 +11,19 @@ namespace Threads
     {
         static void Main(string[] args)
         {
+            Thread th=new Thread((() =>
+            {
+                for (int i = 0; i < 300; i++)
+                {
+                    Console.WriteLine("YYY");
+                }
+            }));
+
+            th.Start();
+            th.Join();
+
+            Console.WriteLine("th is end");
+            Console.ReadLine();
         }
     }
 }
