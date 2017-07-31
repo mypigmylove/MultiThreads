0c5f2e3fb8bf688aab1f92e73e4ce478cc5f93c9
mypigmylove <yxxb@163.com>
2017-07-31 11:19:49 +0800

    yangxingtest

--- MultiThreads/Threads/Program.cs
+++ MultiThreads/Threads/Program.cs
@@ -11,7 +11,7 @@ namespace Threads
     {
         static void Main(string[] args)
         {
-            Thread th=new Thread((() =>
+            Thread th = new Thread((() =>
             {
                 for (int i = 0; i < 300; i++)
                 {
@@ -21,6 +21,49 @@ namespace Threads
 
             th.Start();
             th.Join();
+
+            Task task = Task.Run(() =>
+            {
+                Thread.Sleep(5000);
+                Console.WriteLine("Foo");
+            });
+
+            Console.WriteLine(task.IsCompleted);
+            task.Wait();
+
+            Task<int> taskInt = Task.Run(() =>
+                {
+                    Thread.Sleep(10000);
+                    Console.WriteLine("Foll2");
+                    return 3;
+                });
+
+            var waiter=taskInt.GetAwaiter();
+
+            waiter.OnCompleted(() =>
+            {
+                int result = waiter.GetResult();
+                Console.WriteLine(waiter.IsCompleted);
+
+            });
+
+            taskInt.ContinueWith((task1 =>
+            {
+                int result1 = task1.Result;
+                Console.WriteLine(result1);
+
+            }));
+
+            var tcs = new TaskCompletionSource<int>();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(5000);
+                tcs.SetResult(54);
+            }).Start();
+
+            var qq = tcs.Task;
+            Console.WriteLine(qq.GetAwaiter().GetResult());
 
             Console.WriteLine("th is end");
             Console.ReadLine();
